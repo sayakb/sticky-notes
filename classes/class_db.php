7982758979a7c0349e94c8526700fb7fb14f9856
Sayak Banerjee <sayakb@kde.org>
2011-05-24 23:21:29 +0530

    Made code magic_quotes_gpc agnostic

--- classes/class_db.php
+++ classes/class_db.php
@@ -105,6 +105,7 @@ class db
     function escape(&$data)
     {
         $data = mysql_real_escape_string($data);
+        $data = get_magic_quotes_gpc() ? stripslashes($data) : $data;
     }
 
     // Function to unescape the characters in a string
