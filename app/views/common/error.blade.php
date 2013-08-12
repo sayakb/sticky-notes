dbc9657b818e7071ad16eb668f9ffaf7153db0a2
Sayak Banerjee <sayakb@kde.org>
2013-08-12 04:48:36 -0500

    Error page enclosed in a row

--- app/views/common/error.blade.php
+++ app/views/common/error.blade.php
@@ -1,7 +1,11 @@
 @extends('common.page')
 
 @section('content')
-	<div class="jumbotron">
-		<h1>{{ Lang::get('errors.'.$errCode) }}</h1>
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="jumbotron">
+				<h1>{{ Lang::get('errors.'.$errCode) }}</h1>
+			</div>
+		</div>
 	</div>
 @stop
