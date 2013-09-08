adb0b21edf8af6b1a7c4b432518e5c15d682f36d
Sayak Banerjee <sayakb@kde.org>
2013-09-08 00:34:58 -0500

    Delete theme resources

--- app/views/skins/bootstrap/ajax/version/ok.blade.php
+++
@@ -1,4 +0,0 @@
-<span class="glyphicon glyphicon-ok-sign text-success"
-      title="{{ Lang::get('ajax.version_ok') }}"
-      data-toggle="tooltip">
-</span>
--- app/views/skins/bootstrap/ajax/version/old.blade.php
+++
@@ -1,4 +0,0 @@
-<span class="glyphicon glyphicon-exclamation-sign text-danger"
-      title="{{ Lang::get('ajax.version_old') }}"
-      data-toggle="tooltip">
-</span>
