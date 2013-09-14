b46c9b4944c3bdf5b3ae86f901fbbeffce958425
Sayak Banerjee <sayakb@kde.org>
2013-09-14 16:43:25 -0500

    Initial commit for Neverland skin

---
+++ app/views/skins/neverland/setup/install/stage1.blade.php
@@ -0,0 +1,26 @@
+@extends('skins.bootstrap.setup.page')
+
+@section('body')
+	<section id="install">
+		@if (empty($error))
+			<div class="alert alert-info">
+				{{ Lang::get('setup.welcome') }}
+			</div>
+		@endif
+
+		<fieldset>
+			<legend>{{ Lang::get('setup.i_stage1_title') }}</legend>
+
+			<p>{{ Lang::get('setup.i_stage1_exp') }}</p>
+
+			<p>{{ Lang::get('setup.click_check') }}</p>
+
+			{{
+				Form::submit(Lang::get('setup.test_connection'), array(
+					'name'    => '_test',
+					'class'   => 'btn btn-primary'
+				))
+			}}
+		</fieldset>
+	</section>
+@stop
