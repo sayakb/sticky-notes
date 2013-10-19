20e8f23ea1eb84fb19ddbcd4e9bc533d1a3493bc
Sayak Banerjee <sayakb@kde.org>
2013-10-19 18:03:10 -0500

    Add syndication to template

--- app/views/skins/neverland/common/page.blade.php
+++ app/views/skins/neverland/common/page.blade.php
@@ -6,6 +6,8 @@
 	<meta charset="utf-8">
 	<title>{{ $site->general->title }}</title>
 
+	<link rel="alternate" type="application/rss+xml" title="RSS" href="{{ url('feed') }}" />
+
 	<link href="//cdn.kde.org/img/favicon.png" rel="shortcut icon" />
 	<link href="//cdn.kde.org/css/bootstrap.css" rel="stylesheet" />
 	<link href="//cdn.kde.org/css/bootstrap-responsive.css" rel="stylesheet" />
