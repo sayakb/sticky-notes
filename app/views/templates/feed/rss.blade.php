6158c3b6ddf41d653cb7431353a942d577c3c312
Sayak Banerjee <sayakb@kde.org>
2013-10-19 18:54:20 -0500

    Fix the rss version

--- app/views/templates/feed/rss.blade.php
+++ app/views/templates/feed/rss.blade.php
@@ -1,6 +1,5 @@
 {{ '<?xml version="1.0" encoding="UTF-8"?>' }}
-
-<rss version="2.0">
+<rss version="0.92">
 	<channel>
 		<title>{{ Lang::get('global.feed') }} - {{ $site->general->title }}</title>
 		<link>{{ url('all') }}</link>
