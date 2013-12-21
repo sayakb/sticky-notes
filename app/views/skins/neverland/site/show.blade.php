3a36eb62fe3c42634c03653b26efbd82523b6fb4
Sayak Banerjee <sayakb@kde.org>
2013-12-21 00:04:51 -0600

    Shorten comment info

--- app/views/skins/neverland/site/show.blade.php
+++ app/views/skins/neverland/site/show.blade.php
@@ -60,7 +60,9 @@
 								</div>
 
 								<div class="muted">
-									{{{ sprintf(Lang::get('global.posted_by'), $comment->author, date('d M Y, H:i:s e', $comment->timestamp)) }}}
+									{{{ $comment->author }}}
+									&bull;
+									{{{ date('d M Y, H:i:s e', $comment->timestamp)) }}}
 								</div>
 							</small>
 						</div>
