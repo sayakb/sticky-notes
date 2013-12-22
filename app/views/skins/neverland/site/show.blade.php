44252ecd44ec0429791a6992c6a75cde3cb71129
Sayak Banerjee <sayakb@kde.org>
2013-12-21 20:03:26 -0600

    Regarrangement and regression fixes

--- app/views/skins/neverland/site/show.blade.php
+++ app/views/skins/neverland/site/show.blade.php
@@ -6,6 +6,71 @@
 	<section id="show">
 		@include('skins.neverland.site.paste')
 
+		<div class="row-fluid">
+			<div class="span12">
+				@if ($revisions->count() > 0)
+					<h4>
+						<i class="icon-time icon-blue"></i>
+						{{ Lang::get('show.version_history') }}
+					</h4>
+
+					<fieldset class="well well-small well-white well-history">
+						<div class="viewport">
+							<table class="table table-striped table-responsive">
+								<colgroup>
+									<col class="span3" />
+									<col class="span3" />
+									<col class="span5" />
+									<col class="span1" />
+								</colgroup>
+
+								<thead>
+									<tr>
+										<th>{{ Lang::get('show.revision_id') }}</th>
+										<th>{{ Lang::get('global.author') }}</th>
+										<th>{{ Lang::get('show.created_at') }}</th>
+										<th></th>
+									</tr>
+								</thead>
+
+								<tbody>
+									@foreach ($revisions as $revision)
+										<tr>
+											<td>
+												{{
+													link_to($revision->urlkey, $revision->urlkey)
+												}}
+											</td>
+
+											<td>
+												{{{
+													$paste->author ?: Lang::get('global.anonymous')
+												}}}
+											</td>
+
+											<td>
+												{{
+													date('d M Y, H:i:s e', $revision->timestamp)
+												}}
+											</td>
+
+											<td class="align-right">
+												{{
+													link_to("diff/{$revision->urlkey}/{$paste->urlkey}", Lang::get('show.diff'), array(
+														'class' => 'btn btn-mini btn-default'
+													))
+												}}
+											</td>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
+					</fieldset>
+				@endif
+			</div>
+		</div>
+
 		@if ($site->general->comments)
 			<div class="row-fluid">
 				<div class="span12">
@@ -42,8 +107,8 @@
 					{{ Form::hidden('id', $paste->id) }}
 					{{ Form::close() }}
 
-					@if (count($paste->comments) > 0)
-						@foreach ($paste->comments as $comment)
+					@if ($comments->count() > 0)
+						@foreach ($comments as $comment)
 							<div class="well well-small well-white">
 								<p>
 									{{{ $comment->data }}}
@@ -68,74 +133,11 @@
 								</small>
 							</div>
 						@endforeach
+
+						{{ $comments->links() }}
 					@endif
 				</div>
 			</div>
 		@endif
-
-		<div class="row-fluid">
-			<div class="span12">
-				@if (count($paste->revisions) > 0)
-					<h4>
-						<i class="icon-time icon-blue"></i>
-						{{ Lang::get('show.version_history') }}
-					</h4>
-
-					<fieldset class="well well-small well-white well-history">
-						<div class="viewport">
-							<table class="table table-striped table-responsive">
-								<colgroup>
-									<col class="span3" />
-									<col class="span3" />
-									<col class="span5" />
-									<col class="span1" />
-								</colgroup>
-
-								<thead>
-									<tr>
-										<th>{{ Lang::get('show.revision_id') }}</th>
-										<th>{{ Lang::get('global.author') }}</th>
-										<th>{{ Lang::get('show.created_at') }}</th>
-										<th></th>
-									</tr>
-								</thead>
-
-								<tbody>
-									@foreach ($paste->revisions as $revision)
-										<tr>
-											<td>
-												{{
-													link_to($revision->urlkey, $revision->urlkey)
-												}}
-											</td>
-
-											<td>
-												{{{
-													$paste->author ?: Lang::get('global.anonymous')
-												}}}
-											</td>
-
-											<td>
-												{{
-													date('d M Y, H:i:s e', $revision->timestamp)
-												}}
-											</td>
-
-											<td class="align-right">
-												{{
-													link_to("diff/{$revision->urlkey}/{$paste->urlkey}", Lang::get('show.diff'), array(
-														'class' => 'btn btn-mini btn-default'
-													))
-												}}
-											</td>
-										</tr>
-									@endforeach
-								</tbody>
-							</table>
-						</div>
-					</fieldset>
-				@endif
-			</div>
-		</div>
 	</section>
 @stop
