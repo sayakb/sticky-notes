19e8dde18e8b2555d22dad1ad03254cf8e3cd4d5
Sayak Banerjee <sayakb@kde.org>
2014-04-06 19:39:36 -0500

    Fix issue #94: Add viewport instructions

--- app/views/skins/neverland/setup/page.blade.php
+++ app/views/skins/neverland/setup/page.blade.php
@@ -4,6 +4,7 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>{{ Lang::get('global.sticky_notes') }}</title>
 
 	<link href="//cdn.kde.org/img/favicon.ico" rel="shortcut icon" />
